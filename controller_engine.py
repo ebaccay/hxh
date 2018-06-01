86ffedd132f32f3e82f5829a004e0888f7585c15
Ervin Baccay <ebaccay@berkeley.edu>
2018-06-01 07:10:42 -0700

    added some TODOs for later

--- controller_engine.py
+++ controller_engine.py
@@ -1,4 +1,10 @@
+# TODO: have the button engine and physics engine communicate
+# TODO: eventually have keyboard inputs affecting physical coordinates
+# TODO: add basic "throw" attack in order to test out theta-magnitude form as well as hitbox params
+# NOTE: the third one may need a rudimentary character engine; we'll see
+
 import button_engine as be
+import  physics_engine as pe
 
 
 def main():
